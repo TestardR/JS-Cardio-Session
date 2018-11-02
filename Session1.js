0d134e2d6bd150cb4c13aaecc77b96b0362c3b3d
Romain <rmtestard@gmail.com>
2018-11-02 16:28:05 +0100

    Reversals, MaxChar, FizzBuzz

--- Session1.js
+++ Session1.js
@@ -169,3 +169,19 @@ function maxCharacter(str) {
   }
   return maxChar;
 }
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multpiles of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Buzz');
+    } else if (i % 5 === 0) {
+      console.log('Fizz');
+    } else {
+      console.log(i);
+    }
+  }
+}
