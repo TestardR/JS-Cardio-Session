a58479a15a3a87e741d6f0a0412f1df18109d498
Romain <rmtestard@gmail.com>
2018-11-02 16:20:47 +0100

    max character

--- Session1.js
+++ Session1.js
@@ -144,3 +144,28 @@ function capitalizeLetters(str) {
     return char.toUpperCase();
   });
 }
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript) == 'a'
+
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+  str.split('').forEach(function(char) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
