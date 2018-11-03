ba2938f00b64c19f7170b35f7ce3cbba5b94b901
Romain <rmtestard@gmail.com>
2018-11-03 10:25:40 +0100

    LongestWord, Anagrams, ArrayChunking

--- session2.js
+++ session2.js
@@ -138,15 +138,36 @@ function flattenArray(arrays) {
 // ex. 'Dormitory' === 'ditry room##'
 
 function isAnagram(str1, str2) {
-  return ormatStr(str1) === formatStr(str2);
+  return formatStr(str1) === formatStr(str2);
 }
 
 // Helper function
-function formatStr() {
+function formatStr(str) {
   return str
-    .replace(/[^\w]/g, '')
+    .replace(/[^\w]/g, '') // replace anything that is not a word character.
     .toLowerCase()
     .split('')
     .sort()
     .join('');
+}
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+
+function letterChangers(str) {
+  let newStr = str.toLowerCase().replace(/[a-z]/gi, function(char) {
+    if (char === 'z' || char === 'Z') {
+      return 'a';
+    } else {
+      return String.fromCharCode(char.charCodeAt() + 1);
+    }
+  });
+
+  newStr = newStr.replace(/a|e|i|o|u|y/gi, function(vowel) {
+    return vowel.toUpperCase();
+  });
+
+  return newStr;
 }
