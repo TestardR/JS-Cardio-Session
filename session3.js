065482b0339f124d16d4b524a2e0a97c6b48ff54
Romain <rmtestard@gmail.com>
2018-11-03 15:07:30 +0100

    missingLetters

--- session3.js
+++ session3.js
@@ -119,3 +119,16 @@ function sortByHeight(a) {
 // ex.
 // missingLetters("abce") == "d";
 // missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
+
+function missingLetters(str) {
+  let compare = str.charCodeAt(0);
+  let missing;
+  str.split('').map((char, i) => {
+    if (str.charCodeAt(i) == compare) {
+      ++compare;
+    } else {
+      missing = String.fromCharCode(compare);
+    }
+  });
+  return missing;
+}
