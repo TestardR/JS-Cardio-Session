fd62417c7c2be2368d0efee909c8a0c4385a5db8
Romain <rmtestard@gmail.com>
2018-11-10 09:06:09 +0100

    map, filter, sort, reduce

---
+++ session5.js
@@ -0,0 +1,154 @@
+const companies = [
+  { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+  { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+  { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+  { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+  { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+  { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+  { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+  { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+  { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 }
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// CHALLENGE 1 : GET ALL COMPANIES
+
+// standard for loop
+// for(let i = 0; i < companies.length; i++){
+// 	console.log(companies[i])
+// }
+
+// forEach
+
+// companies.forEach(function(company) {
+//   console.log(company);
+// });
+
+// CHALLENGE 2 : filter only 21 and over
+// filter
+
+// let canDrink = [];
+// for (let i = 0; i < ages.length; i++) {
+//   if (ages[i] >= 21) {
+//     canDrink.push(ages[i]);
+//   }
+// }
+
+// const canDrink = ages.filter(function(age) {
+//   if (age >= 21) {
+//     return true;
+//   }
+// });
+
+// const canDrink = ages.filter(age => age >= 21);
+
+// console.log(canDrink);
+
+// CHALLENGE 3 : filter the retail companies
+
+// const retailCompanies = companies.filter(function(company) {
+//   if (company.category === 'Retail') {
+//     return true;
+//   }
+// });
+
+// const retailcompanies = companies.filter(
+//   company => company.category === 'Retail'
+// );
+
+// CHALLENGE 4: filter companies from the 80's
+
+// const eightiesCompanies = companies.filter(function(company) {
+//   if (company.start >= 1980 && company.start < 1990) {
+//     return true;
+//   }
+// });
+
+// const eightiesCompanies = companies.filter(
+//   company => company.start >= 1980 && company.start < 1990
+// );
+
+// CHALLENGE 5: filter comanies that laster for at least for 10 years
+
+// const lastedTenYears = companies.filter(
+//   company => company.end - company.start >= 10
+// );
+
+// console.log(lastedTenYears);
+
+// map
+// CHALLENGE 6: Create array of company names
+// const companyNames = companies.map(function(company) {
+//   return company.name;
+// });
+
+// const companyNames = companies.map(company => company.name);
+
+// CHALLENGE 7: Create array for company names with start and end date
+const companiesBio = companies.map(
+  company => `${company.name} [${company.start} - ${company.end}]`
+);
+
+// CHALLENGE 8: Create array of the ages' squareroot
+const agesSquare = ages.map(age => Math.sqrt(age));
+
+// CHALLENGE 9: Create array of the ages' squareroot times 2
+const agesSquareTimesTwo = ages.map(age => Math.sqrt(age)).map(age => age * 2);
+
+// sort
+// CHALLENGE 10: Sort companies by creation date
+// const sortedCompanies = companies.sort(function(c1, c2) {
+//   if (c1.start > c2.start) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+// const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+
+// console.log(sortedCompanies);
+
+// CHALLENGE 11: Sort ages in ascending order
+
+// const sortedAges = ages.sort((a, b) => (a > b ? 1 : -1));
+// console.log(sortedAges);
+
+// reduce
+// CHALLENGE 12: Add all the numbers in the ages' array
+// let ageSum = 0;
+// for (let i = 0; i < ages.length; i++) {
+//   ageSum += ages[i];
+// }
+
+// const ageSum = ages.reduce(function(total, age) {
+//   return total + age;
+// }, 0);
+
+// const ageSum = ages.reduce((total, age) => total + age, 0);
+
+// console.log(ageSum);
+
+// CHALLENGE 13: Get the total years of existence for all companies
+
+// const totalYears = companies.reduce(function(total, company) {
+//   return total + (company.end - company.start);
+// }, 0);
+
+// const totalYears = companies.reduce(
+//   (total, company) => total + (company.end - company.start),
+//   0
+// );
+
+// console.log(totalYears);
+
+// CHALLENGE 14: Combine methods
+
+// const combined = ages
+//   .map(age => age * 2)
+//   .filter(age => age >= 40)
+//   .sort((a, b) => a - b)
+//   .reduce((a, b) => a + b, 0);
+
+// console.log(combined);
